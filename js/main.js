cd715c155023c588f21a23a0f3c352ee36be018c
ManuelMaganaL <manuel.maganal@uanl.edu.mx>
2025-03-31 21:55:17 -0600

    Requerimiento: Mejora en la interfaz grafica

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@
 function showSection(sectionId) {
     const sections = document.querySelectorAll("main section");
     sections.forEach(section => {
-      section.classList.remove("active");
+        section.classList.remove("active");
     });
     document.getElementById(sectionId).classList.add("active");
 }
-  
+
 function calcular(operacion) {
     const num1 = parseFloat(document.getElementById(`num1-${operacion}`).value);
     const num2 = parseFloat(document.getElementById(`num2-${operacion}`).value);
     let resultado;
-  
+
     switch (operacion) {
-      case "suma":
+        case "suma":
         resultado = num1 + num2;
         break;
-      case "resta":
+        case "resta":
         resultado = num1 - num2;
         break;
-      case "multiplicacion":
+        case "multiplicacion":
         resultado = num1 * num2;
         break;
-      case "division":
+        case "division":
         resultado = num1 / num2;
         break;
-      default:
+        default:
         resultado = "Operación no válida";
     }
-  
+
     document.getElementById(`resultado-${operacion}`).textContent = `Resultado: ${resultado}`;
 }
